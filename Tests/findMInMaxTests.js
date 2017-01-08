ed832745577760a47ca5471701e1b7d9c99989b3
Itunu Fatoki <itunuworks@gmail.com>
2017-01-08 22:08:04 +0100

    This was after better refactoring my test codes.

--- Tests/findMInMaxTests.js
+++ Tests/findMInMaxTests.js
@@ -2,34 +2,34 @@
 
 'use strict'
 
-var myApp = require('../app/main.js');
+var findMinMax = require('../app/main.js').findMinMax;
 
 describe('Min-Max Numbers in a List: ', function () {
 
   describe('Return the min and max number in the list in a new list follows `[min, max]`', function () {
 
     it('should return [1,4] for [1, 2, 3 , 4]', function () {
-      expect(myApp.findMinMax([1, 2, 3, 4])).toEqual([1, 4]);
+      expect(findMinMax([1, 2, 3, 4])).toEqual([1, 4]);
     });
 
     it('should return [4, 6] for [6, 4]', function () {
-      expect(myApp.findMinMax([6, 4])).toEqual([4, 6]);
+      expect(findMinMax([6, 4])).toEqual([4, 6]);
     });
 
     it('should return [2, 78] for [4, 66, 6, 44, 7, 78, 8, 68, 2]', function () {
-      expect(myApp.findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).toEqual([2, 78]);
+      expect(findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).toEqual([2, 78]);
     });
 
     it('should return [-1, 1024] for [1024, -1, 4, 2, 9]', function () {
-      expect(myApp.findMinMax([1024, -1, 4, 2, 9])).toEqual([-1, 1024]);
+      expect(findMinMax([1024, -1, 4, 2, 9])).toEqual([-1, 1024]);
     });
 
     it('should return [55, 78] for [65, 57, 55, 78]', function () {
-      expect(myApp.findMinMax([65, 57, 55, 78])).toEqual([55, 78]);
+      expect(findMinMax([65, 57, 55, 78])).toEqual([55, 78]);
     });
 
     it('should return [14, 16] for [16, 14]', function () {
-      expect(myApp.findMinMax([16, 14])).toEqual([14, 16]);
+      expect(findMinMax([16, 14])).toEqual([14, 16]);
     });
 
   });
@@ -37,23 +37,23 @@ describe('Min-Max Numbers in a List: ', function () {
   describe('Return the number in the list in a new list follows `[min]` when the number is the min and max number in that list', function () {
 
     it('should return [4] for [4, 4, 4, 4]', function () {
-      expect(myApp.findMinMax([4, 4, 4, 4])).toEqual([4]);
+      expect(findMinMax([4, 4, 4, 4])).toEqual([4]);
     });
 
     it('should return [3] for [3, 3, 3, 3, 3]', function () {
-      expect(myApp.findMinMax([3, 3, 3, 3, 3])).toEqual([3]);
+      expect(findMinMax([3, 3, 3, 3, 3])).toEqual([3]);
     });
 
     it('should return [100] for [100, 100, 100]', function () {
-      expect(myApp.findMinMax([100, 100, 100])).toEqual([100]);
+      expect(findMinMax([100, 100, 100])).toEqual([100]);
     });
 
     it('should return [200] for [200]', function () {
-      expect(myApp.findMinMax([200])).toEqual([200]);
+      expect(findMinMax([200])).toEqual([200]);
     });
 
     it('should return [15] for [15, 15]', function () {
-      expect(myApp.findMinMax([15, 15])).toEqual([15]);
+      expect(findMinMax([15, 15])).toEqual([15]);
     });
 
   });
--- Tests/fizzBuzzTests.js
+++ Tests/fizzBuzzTests.js
@@ -1,54 +1,54 @@
 'use strict'
 
-var myApp = require('../app/main.js');
+var fizzBuzz = require('../app/main.js').fizzBuzz;
 
 describe("Fizz Buzz tests ", function() {
 
   it("should return `Fizz` for number divisible by 3", function() {
-    expect(myApp.fizzBuzz(3)).toBe('Fizz');
+    expect(fizzBuzz(3)).toBe('Fizz');
   });
 
   it("should return `Buzz` for number divisible by 5", function() {
-    expect(myApp.fizzBuzz(5)).toBe('Buzz');
+    expect(fizzBuzz(5)).toBe('Buzz');
   });
 
   it("should return `FizzBuzz` for 15", function() {
-    expect(myApp.fizzBuzz(15)).toBe('FizzBuzz');
+    expect(fizzBuzz(15)).toBe('FizzBuzz');
   });
 
   it("should return `FizzBuzz` for 45", function() {
-    expect(myApp.fizzBuzz(45)).toBe('FizzBuzz');
+    expect(fizzBuzz(45)).toBe('FizzBuzz');
   });
 
   it("should return `FizzBuzz` for 90", function() {
-    expect(myApp.fizzBuzz(90)).toBe('FizzBuzz');
+    expect(fizzBuzz(90)).toBe('FizzBuzz');
   });
 
   it("should return `Fizz` for 63", function() {
-    expect(myApp.fizzBuzz(63)).toBe('Fizz');
+    expect(fizzBuzz(63)).toBe('Fizz');
   });
 
   it("should return 7 since its indivisible by 3 and 5", function() {
-    expect(myApp.fizzBuzz(7)).toBe(7);
+    expect(fizzBuzz(7)).toBe(7);
   });
 
   it("should return 101 since its indivisible by 3 and 5", function() {
-    expect(myApp.fizzBuzz(101)).toBe(101);
+    expect(fizzBuzz(101)).toBe(101);
   });
 
   it("should return `19` for number indivisible by 3 and 5", function() {
-    expect(myApp.fizzBuzz(19)).toBe(19);
+    expect(fizzBuzz(19)).toBe(19);
   });
 
   it("should return `FizzBuzz` for 60", function() {
-    expect(myApp.fizzBuzz(60)).toBe('FizzBuzz');
+    expect(fizzBuzz(60)).toBe('FizzBuzz');
   });
 
   it("should return `Buzz` for 1105", function() {
-    expect(myApp.fizzBuzz(1105)).toBe('Buzz');
+    expect(fizzBuzz(1105)).toBe('Buzz');
   });
 
   it("should return `Fizz` for 999", function() {
-    expect(myApp.fizzBuzz(999)).toBe('Fizz');
+    expect(fizzBuzz(999)).toBe('Fizz');
   });
 })
